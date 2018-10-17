628ee2eda9ea5877589bb56b796b46764e6a5943
McLeanResearchGroup <picache.j@gmail.com>
2018-10-17 11:34:02 -0500

    20181017JAP

--- Shiny/server.R
+++ Shiny/server.R
@@ -2,7 +2,6 @@ library(shiny)
 library(shinythemes)
 library(plotly)
 library(DT)
-library(pdftools)
 
 #setwd("/Users/JAPicache/Box Sync/R_Scripts&Data/20171218JAP_iceberg/Shiny/CCScompendium_Shiny")
 
@@ -39,49 +38,51 @@ eq <- c('<img src = "http://quicklatex.com/cache3/22/ql_81062506f2cd83637777e4c5
         '<img src = "http://quicklatex.com/cache3/37/ql_fcb4318d5a05c209d237d5a7124ff237_l3.png"></img>', #Benzene and substituted derivatives
         '<img src = "http://quicklatex.com/cache3/b3/ql_c0b831211948b883465cb5656af8fbb3_l3.png"></img>', #Carboxylic acids and derivatives
         '<img src = "http://quicklatex.com/cache3/6b/ql_5eb3ac234c6fa316159dc24e217e1d6b_l3.png"></img>', #Diazines
-        '<img src = "http://quicklatex.com/cache3/19/ql_260f8b5f1cea0f00a875dcdb7e0f9719_l3.png"></img>', #Fatty acyls
+        '<img src = "http://quicklatex.com/cache3/43/ql_de636283c521608892da5a602c6ca143_l3.png"></img>', #Fatty acyls
         '<img src = "http://quicklatex.com/cache3/37/ql_42b3d40039f262212131cc5af9ec4637_l3.png"></img>', #Glycerophospholipids
         '<img src = "http://quicklatex.com/cache3/c6/ql_f27501280413c0ec44ff7075885731c6_l3.png"></img>', #Homogeneous transition metal compounds
-        '<img src = "http://quicklatex.com/cache3/83/ql_81c75300afd61a0c95a71408fe184983_l3.png"></img>', #Naphthofurans
+        '<img src = "http://quicklatex.com/cache3/39/ql_9b01162a5977bd7c91ec7d2738aa8739_l3.png"></img>', #Naphthofurans
         '<img src = "http://quicklatex.com/cache3/81/ql_ca1fd309c6ee6f37b111ddcf2fa2ad81_l3.png"></img>', #Organofluorides
         '<img src = "http://quicklatex.com/cache3/9c/ql_7d3fdd43c6a05167ebce991e8181ea9c_l3.png"></img>', #Organooxygen compounds
         '<img src = "http://quicklatex.com/cache3/51/ql_26698cc691418e860e8526eb4e70b851_l3.png"></img>', #Peptidomimetics
-        '<img src = "http://quicklatex.com/cache3/27/ql_ca0a96465c0cd1c0051c3c809bce1627_l3.png"></img>', #Proteins
-        '<img src = "http://quicklatex.com/cache3/c1/ql_56fb901e426b135860b22685efe4bec1_l3.png"></img>', #Pteridines and derivatives
+        '<img src = "http://quicklatex.com/cache3/6f/ql_da8a8bb86e9e45a7a6ea126db562c36f_l3.png"></img>', #Proteins
+        '<img src = "http://quicklatex.com/cache3/f1/ql_484eac97c5b549b9e6b0df776977baf1_l3.png"></img>', #Pteridines and derivatives
         '<img src = "http://quicklatex.com/cache3/4b/ql_9930ab424cd16b64d59e9bd7a4a57e4b_l3.png"></img>', #Pyridines and derivatives
         '<img src = "http://quicklatex.com/cache3/f2/ql_f1103e288c90ec6e9cc1494380b22af2_l3.png"></img>', #Tryptic peptides
         '<img src = "http://quicklatex.com/cache3/ff/ql_878fcb94810be61b8493842684af5aff_l3.png"></img>', #Imidazopyrimidines
         '<img src = "http://quicklatex.com/cache3/98/ql_249b6ce3c8df0bde309168b98b16be98_l3.png"></img>', #Isoflavonoids
-        '<img src = "http://quicklatex.com/cache3/13/ql_1053025ca38b3b63e82d666b28b8be13_l3.png"></img>', #Macrolides and analogues
-        '<img src = "http://quicklatex.com/cache3/b3/ql_d4e098c88ac33915be3bd549758587b3_l3.png"></img>', #Organonitrogen compounds
-        '<img src = "http://quicklatex.com/cache3/6b/ql_ddfc8e2336e1116f60853ed04c64d46b_l3.png"></img>', #Prenol lipids
-        '<img src = "http://quicklatex.com/cache3/30/ql_3a53ee6a48add984274e43ea01940230_l3.png"></img>', #Purine nucleotides
-        '<img src = "http://quicklatex.com/cache3/f1/ql_06b741df5fe59b89b3afa8c9d74beff1_l3.png"></img>', #Pyrenes
-        '<img src = "http://quicklatex.com/cache3/0b/ql_6146fa5cfbb480da4acdea71a3c1330b_l3.png"></img>', #Pyrimidine nucleotides
-        '<img src = "http://quicklatex.com/cache3/56/ql_863780f56e78d2947fa07822cf4f2456_l3.png"></img>', #Quinolines and derivatives
-        '<img src = "http://quicklatex.com/cache3/fd/ql_98659bfd3be0ef1c62ebcfc0274ca3fd_l3.png"></img>', #Organooxygen compounds/ Alcohols and polyols
-        '<img src = "http://quicklatex.com/cache3/db/ql_ad046820f9c32bd21b3239b720348fdb_l3.png"></img>', #Carboxylic acids and derivatives/ Amino acids, peptides, and analogues
-        '<img src = "http://quicklatex.com/cache3/d6/ql_dbd57ee06fae7b76143c78f9b30277d6_l3.png"></img>', #Flavonoids/ Flavonoid glycosides
-        '<img src = "http://quicklatex.com/cache3/d0/ql_2770c5b8ba43afb82674d5aaec083dd0_l3.png"></img>', #Glycerophospholipids/ Glycerophosphates
-        '<img src = "http://quicklatex.com/cache3/6d/ql_11f14ff77f76b58752b5afa3752ca06d_l3.png"></img>', #Glycerophospholipids/ Glycerophosphocholines
-        '<img src = "http://quicklatex.com/cache3/b5/ql_2a312fdcd53027ecf62a9ecd7c352db5_l3.png"></img>', #Glycerophospholipids/ Glycerophosphoethanolamines
-        '<img src = "http://quicklatex.com/cache3/81/ql_ca1fd309c6ee6f37b111ddcf2fa2ad81_l3.png"></img>', #Organofluorides/ Phosphazene and phosphazene derivatives
-        '<img src = "http://quicklatex.com/cache3/95/ql_e4455f650618a88c51ee56f152497a95_l3.png"></img>', #Pyrimidine nucleotides/ Pyrimidine ribonucleotides
-        '<img src = "http://quicklatex.com/cache3/6b/ql_5eb3ac234c6fa316159dc24e217e1d6b_l3.png"></img>', #Diazines/ Pyrimidines and pyrimidine derivatives
-        '<img src = "http://quicklatex.com/cache3/a3/ql_15062458967a66323e405c8b649e42a3_l3.png"></img>', #Organonitrogen compounds/ Quaternary ammonium salts
-        '<img src = "http://quicklatex.com/cache3/b0/ql_4f4745f1a579c17c479b7d1d3c6eefb0_l3.png"></img>', #5'-deoxyribonucleosides/ 5'-deoxy-5'-thionucleosides
-        '<img src = "http://quicklatex.com/cache3/82/ql_7332d9b3e8babe45607cb2f025497b82_l3.png"></img>', #Organonitrogen compounds/ Amines
-        '<img src = "http://quicklatex.com/cache3/31/ql_aafcb441036d66bf5643de5f944f9c31_l3.png"></img>', #Benzene and substituted derivatives/ Benzenesulfonamides
-        '<img src = "http://quicklatex.com/cache3/63/ql_236a3c66142a2a90e0786fdb3e6ad363_l3.png"></img>', #Steroids and steroid derivatives/ Bile acids, alcohols and derivatives
-        '<img src = "http://quicklatex.com/cache3/1b/ql_18d9906ca21282f4932147ff43d2751b_l3.png"></img>', #Benzene and substituted derivatives/ Biphenyls and derivatives
-        '<img src = "http://quicklatex.com/cache3/81/ql_df932fd3ddf6201ac42ee44698125681_l3.png"></img>', #Organooxygen compounds/ Carbohydrates and carbohydrate conjugates
-        '<img src = "http://quicklatex.com/cache3/f4/ql_56d54dda8d73218d71c92766e5a292f4_l3.png"></img>', #Organooxygen compounds/ Carbonyl compounds
-        '<img src = "http://quicklatex.com/cache3/e4/ql_030af2eaf774679f81201f0322b4e9e4_l3.png"></img>', #Glycerophospholipids/ Glycerophosphoserines
-        '<img src = "http://quicklatex.com/cache3/79/ql_064747b4d9ff328aaa833bf28f4d3679_l3.png"></img>', #Indoles and derivatives/ Indoles
-        '<img src = "http://quicklatex.com/cache3/47/ql_9d8629bd27a1c8f30aecbd4506f61847_l3.png"></img>', #Indoles and derivatives/ Indolyl carboxylic acids and derivatives
-        '<img src = "http://quicklatex.com/cache3/d6/ql_4f1d3c384d52b6ccd8b8b5c62f2cf3d6_l3.png"></img>', #Sphingolipids/ Phosphosphingolipids
-        '<img src = "http://quicklatex.com/cache3/84/ql_577fb705b93d2ba71a93f452380d2e84_l3.png"></img>', #Purine nucleotides/ Purine deoxyribonucleotides
-        '<img src = "http://quicklatex.com/cache3/2f/ql_25ab82ca2bc7df336a4180e1ecdfdf2f_l3.png"></img>', #Purine nucleotides/ Purine nucleotide sugars
+        '<img src = "http://quicklatex.com/cache3/0b/ql_6c3cf8371782c261bbbb4de42203720b_l3.png"></img>', #Macrolides and analogues
+        '<img src = "http://quicklatex.com/cache3/13/ql_1053025ca38b3b63e82d666b28b8be13_l3.png"></img>', #Organonitrogen compounds
+        '<img src = "http://quicklatex.com/cache3/b3/ql_d4e098c88ac33915be3bd549758587b3_l3.png"></img>', #Prenol lipids
+        '<img src = "http://quicklatex.com/cache3/6b/ql_ddfc8e2336e1116f60853ed04c64d46b_l3.png"></img>', #Purine nucleotides
+        '<img src = "http://quicklatex.com/cache3/30/ql_3a53ee6a48add984274e43ea01940230_l3.png"></img>', #Pyrenes
+        '<img src = "http://quicklatex.com/cache3/f1/ql_06b741df5fe59b89b3afa8c9d74beff1_l3.png"></img>', #Pyrimidine nucleotides
+        '<img src = "http://quicklatex.com/cache3/0b/ql_6146fa5cfbb480da4acdea71a3c1330b_l3.png"></img>', #Quinolines and derivatives
+        '<img src = "http://quicklatex.com/cache3/56/ql_863780f56e78d2947fa07822cf4f2456_l3.png"></img>', #Organooxygen compounds/ Alcohols and polyols
+        '<img src = "http://quicklatex.com/cache3/fd/ql_98659bfd3be0ef1c62ebcfc0274ca3fd_l3.png"></img>', #Carboxylic acids and derivatives/ Amino acids, peptides, and analogues
+        '<img src = "http://quicklatex.com/cache3/db/ql_ad046820f9c32bd21b3239b720348fdb_l3.png"></img>', #Flavonoids/ Flavonoid glycosides
+        '<img src = "http://quicklatex.com/cache3/d6/ql_dbd57ee06fae7b76143c78f9b30277d6_l3.png"></img>', #Glycerophospholipids/ Glycerophosphates
+        '<img src = "http://quicklatex.com/cache3/d0/ql_2770c5b8ba43afb82674d5aaec083dd0_l3.png"></img>', #Glycerophospholipids/ Glycerophosphocholines
+        '<img src = "http://quicklatex.com/cache3/ff/ql_3cbfad5ded9c420c10ad9a8ab7c4baff_l3.png"></img>', #Glycerophospholipids/ Glycerophosphoethanolamines
+        '<img src = "http://quicklatex.com/cache3/b5/ql_2a312fdcd53027ecf62a9ecd7c352db5_l3.png"></img>', #Organofluorides/ Phosphazene and phosphazene derivatives
+        '<img src = "http://quicklatex.com/cache3/81/ql_ca1fd309c6ee6f37b111ddcf2fa2ad81_l3.png"></img>', #Pyrimidine nucleotides/ Pyrimidine ribonucleotides
+        '<img src = "http://quicklatex.com/cache3/95/ql_e4455f650618a88c51ee56f152497a95_l3.png"></img>', #Diazines/ Pyrimidines and pyrimidine derivatives
+        '<img src = "http://quicklatex.com/cache3/6b/ql_5eb3ac234c6fa316159dc24e217e1d6b_l3.png"></img>', #Organonitrogen compounds/ Quaternary ammonium salts
+        '<img src = "http://quicklatex.com/cache3/a3/ql_15062458967a66323e405c8b649e42a3_l3.png"></img>', #5'-deoxyribonucleosides/ 5'-deoxy-5'-thionucleosides
+        '<img src = "http://quicklatex.com/cache3/b0/ql_4f4745f1a579c17c479b7d1d3c6eefb0_l3.png"></img>', #Organonitrogen compounds/ Amines
+        '<img src = "http://quicklatex.com/cache3/82/ql_7332d9b3e8babe45607cb2f025497b82_l3.png"></img>', #Benzene and substituted derivatives/ Benzenesulfonamides
+        '<img src = "http://quicklatex.com/cache3/31/ql_aafcb441036d66bf5643de5f944f9c31_l3.png"></img>', #Steroids and steroid derivatives/ Bile acids, alcohols and derivatives
+        '<img src = "http://quicklatex.com/cache3/0b/ql_6c3cf8371782c261bbbb4de42203720b_l3.png"></img>', #Benzene and substituted derivatives/ Biphenyls and derivatives
+        '<img src = "http://quicklatex.com/cache3/63/ql_236a3c66142a2a90e0786fdb3e6ad363_l3.png"></img>', #Organooxygen compounds/ Carbohydrates and carbohydrate conjugates
+        '<img src = "http://quicklatex.com/cache3/1b/ql_18d9906ca21282f4932147ff43d2751b_l3.png"></img>', #Organooxygen compounds/ Carbonyl compounds
+        '<img src = "http://quicklatex.com/cache3/81/ql_df932fd3ddf6201ac42ee44698125681_l3.png"></img>', #Glycerophospholipids/ Glycerophosphoserines
+        '<img src = "http://quicklatex.com/cache3/f4/ql_56d54dda8d73218d71c92766e5a292f4_l3.png"></img>', #Indoles and derivatives/ Indoles
+        '<img src = "http://quicklatex.com/cache3/e4/ql_030af2eaf774679f81201f0322b4e9e4_l3.png"></img>', #Indoles and derivatives/ Indolyl carboxylic acids and derivatives
+        '<img src = "http://quicklatex.com/cache3/79/ql_064747b4d9ff328aaa833bf28f4d3679_l3.png"></img>', #Sphingolipids/ Phosphosphingolipids
+        '<img src = "http://quicklatex.com/cache3/47/ql_9d8629bd27a1c8f30aecbd4506f61847_l3.png"></img>', #Purine nucleotides/ Purine deoxyribonucleotides
+        '<img src = "http://quicklatex.com/cache3/d6/ql_4f1d3c384d52b6ccd8b8b5c62f2cf3d6_l3.png"></img>', #Purine nucleotides/ Purine nucleotide sugars
+        '<img src = "http://quicklatex.com/cache3/84/ql_577fb705b93d2ba71a93f452380d2e84_l3.png"></img>', #Imidazopyrimidines/ Purines and purine derivatives
+        '<img src = "http://quicklatex.com/cache3/2f/ql_25ab82ca2bc7df336a4180e1ecdfdf2f_l3.png"></img>', #Imidazopyrimidines/ Purines and purine derivatives
         '<img src = "http://quicklatex.com/cache3/ff/ql_878fcb94810be61b8493842684af5aff_l3.png"></img>' #Imidazopyrimidines/ Purines and purine derivatives
         )
 
@@ -162,7 +163,7 @@ server <- function(input, output, session) {
                                 "<br>CAS: ", data$CAS,
                                 "<br>m/z:", data$mz,
                                 "<br>Charge Species: ", data$Adduct,
-                                "<br>CCS/z:",data$CCS.z,"+/-", data$RSD/data$CCS,
+                                "<br>CCS/z:",data$CCS.z,"+/-", data$SD,
                                 "<br>Class/Subclass: ", data$Class,"/",data$Subclass,
                                 "<br>Source: ", data$Source,
                                 "<br>DOI: ", data$DOI),
@@ -182,8 +183,8 @@ server <- function(input, output, session) {
   )
   
   output$table <- DT::renderDataTable({
-    datatable(all[,c(1:7, 9, 8, 10:15)],
-              colnames = c("Compound", "Formula", "CAS", "m/z", "Adduct", "Charge State", "CCS", "CCS/z", "RSD", "Kingdom", "Super Class", "Class", "Subclass", "Source", "DOI"),
+    datatable(all[,c(1:7, 10, 8:9, 11:16)],
+              colnames = c("Compound", "Formula", "CAS", "m/z", "Adduct", "Charge State", "CCS", "CCS/z", "SD", "RSD", "Kingdom", "Super Class", "Class", "Subclass", "Source", "DOI"),
               options = list(pageLength = 10, autoWidth = TRUE, columnDefs = list(list(width = '100px', targets = c(1, 10:13, 15))))
     )
   })
@@ -238,8 +239,8 @@ server <- function(input, output, session) {
 
 #eq
   output$fitTable <- DT::renderDataTable({
-    datatable(data.frame(ft[, 1:3], eq = eq, ft[, 5]),
-              colnames = c("Class", "Sublcass", "Fit Type", "Regression Equation", "Standard Error"),
+    datatable(data.frame(ft[, 1:3], eq = eq, ft[, 5:6]),
+              colnames = c("Class", "Subclass", "Fit Type", "Regression Equation", "Standard Error", "Standard Deviation"),
               options = list(pageLength = 10, autowidth = FALSE, columnDefs = list(list(className = 'dt-center', targets = 2:5))),
               escape = FALSE)
   })
